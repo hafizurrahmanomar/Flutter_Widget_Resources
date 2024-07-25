b5dda57881b60cacfeb16d8556ae6cf6c7a018dc
hafizurrahmanomar <hafizurrahmanomar@gmail.com>
2024-07-25 06:55:19 +0600

    flutter Button Design Update

--- Flutter_Widget_Resources/12.2-Flutter-Button- Design/main.dart
+++ Flutter_Widget_Resources/12.2-Flutter-Button- Design/main.dart
@@ -29,138 +29,180 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("Flutter Button "),
       ),
-      body: Center(
-        child: (Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              /// ElevatedButton
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.orangeAccent,
+      body: SingleChildScrollView(
+        child: Center(
+          child: (Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                /// ElevatedButton
+                const Text("ElevatedButton"),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orangeAccent,
 
-                  /// Button Background Color
-                  foregroundColor: Colors.white,
+                    /// Button Background Color
+                    foregroundColor: Colors.purple,
 
-                  /// Button Text Color
-                  minimumSize: const Size(300, 40),
+                    /// Button Text Color
+                    minimumSize: const Size(400, 80),
 
-                  /// width = 300 ,height=40,
-                  maximumSize: const Size(300, 60),
-                  padding:
-                  const EdgeInsets.symmetric(horizontal: 20, vertical: 32),
-                  //padding: const EdgeInsets.all(32),
-                  // padding: const EdgeInsets.fromLTRB(32, 10, 32, 10),
-                  // padding: EdgeInsets.zero,
-                  // padding: const EdgeInsets.only(left: 0.0,top: 0.0,right: 0.0,bottom: 30),
-                  elevation: 7,
-                  shadowColor: Colors.deepOrange.shade400,
+                    /// width = 300 ,height=40,
+                    maximumSize: const Size(400, 100),
+                    padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 32),
+                    //padding: const EdgeInsets.all(32),
+                    // padding: const EdgeInsets.fromLTRB(32, 10, 32, 10),
+                    // padding: EdgeInsets.zero,
+                    // padding: const EdgeInsets.only(left: 0.0,top: 0.0,right: 0.0,bottom: 30),
+                    elevation: 7,
+                    shadowColor: Colors.deepOrange.shade400,
 
-                  /// Other Option available
+                    /// Other Option available
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+
+                    /// Button Text Style
+                    textStyle: const TextStyle(
+                      color: Colors.deepOrange,
+
+                      ///  Color No Apply,cause foreground color use
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.w800,
+                      wordSpacing: 10.0,
+                      letterSpacing: 5.0,
+                      decoration: TextDecoration.overline,
+
+                      /// Other Option Available
+                      backgroundColor: Colors.white,
+
+                      /// Button Text Background Color
+                    ),
+                  ),
+                  child: const Text("ElevatedButton"),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+
+                /// TextButton
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+
+                    /// Same to ElevatedButton Style apply.
+                  ),
+                  child: const Text("TextButton"),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                //// Icon Button
+                const Text("Icon Button"),
+                IconButton(
+                  onPressed: () {},
+                  style: IconButton.styleFrom(
+                    // Same To Elevated Button Style
+                      backgroundColor: Colors.orangeAccent),
+                  icon: const Icon(Icons.account_circle),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+
+                ///FloatingActionButton with Label
+                const Text("FloatingActionButton with Label"),
+                FloatingActionButton.extended(
+                  onPressed: () {},
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(8)),
+                  elevation: 8,
+                  backgroundColor: Colors.deepOrange,
+                  isExtended: true,
+                  hoverColor: Colors.white,
+                  autofocus: true,
+                  focusColor: Colors.amber.shade400,
+                  focusElevation: 10,
+                  label: const Text("FloatingActionButton"),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
 
-                  /// Button Text Style
-                  textStyle: const TextStyle(
-                    color: Colors.deepOrange,
+                ///FloatingActionButton with child
+                const Text("FloatingActionButton with child"),
+                FloatingActionButton(
+                  onPressed: () {},
+                  //child: const Text("FloatingActionButton"),
+                  child: const Icon(Icons.add_circle),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
 
-                    ///  Color No Apply,cause foreground color use
-                    fontSize: 35.0,
-                    fontWeight: FontWeight.w800,
-                    wordSpacing: 10.0,
-                    letterSpacing: 5.0,
-                    decoration: TextDecoration.overline,
-
-                    /// Other Option Available
-                    backgroundColor: Colors.white,
-
-                    /// Button Text Background Color
+                ///FilledButton
+                const Text("FilledButton"),
+                FilledButton(
+                  style: FilledButton.styleFrom(
+                    /// same to Elevated Button style follow
+                    backgroundColor: Colors.amber,
                   ),
+                  onPressed: () {},
+                  child: const Text("FilledButton"),
                 ),
-                child: const Text("ElevatedButton"),
-              ),
-
-              /// TextButton
-              TextButton(
-                onPressed: () {},
-                style: TextButton.styleFrom(
-
-                  /// Same to ElevatedButton Style apply.
+                const SizedBox(
+                  height: 15,
                 ),
-                child: const Text("TextButton"),
-              ),
 
-              //// Icon Button
-
-              IconButton(
-                onPressed: () {},
-                style: IconButton.styleFrom(
-                  // Same To Elevated Button Style
-                    backgroundColor: Colors.orangeAccent),
-                icon: const Icon(Icons.account_circle),
-              ),
-
-              ///FloatingActionButton with Label
-              FloatingActionButton.extended(
-                onPressed: () {},
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8)),
-                elevation: 8,
-                backgroundColor: Colors.deepOrange,
-                isExtended: true,
-                hoverColor: Colors.white,
-                autofocus: true,
-                focusColor: Colors.amber.shade400,
-                focusElevation: 10,
-                label: const Text("FloatingActionButton"),
-              ),
-
-              ///FloatingActionButton with child
-              FloatingActionButton(
-                onPressed: () {},
-                //child: const Text("FloatingActionButton"),
-                child: const Icon(Icons.add_circle),
-              ),
-
-              ///FilledButton
-              FilledButton(
-                style: FilledButton.styleFrom(
-                  /// same to Elevated Button style follow
-                  backgroundColor: Colors.amber,
+                /// OutlinedButton
+                const Text("OutlinedButton"),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: const Text("OutlinedButton"),
                 ),
-                onPressed: () {},
-                child: const Text("FilledButton"),
-              ),
+                const SizedBox(
+                  height: 15,
+                ),
+                ////MaterialButton
+                const Text("MaterialButton"),
+                MaterialButton(
+                  onPressed: () {},
+                  child: const Text("MaterialButton"),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
 
-              /// OutlinedButton
-              OutlinedButton(
-                onPressed: () {},
-                child: const Text("OutlinedButton"),
-              ),
+                ///BackButton
+                const Text("BackButton"),
+                BackButton(onPressed: () {}, color: Colors.deepOrange),
+                const SizedBox(
+                  height: 15,
+                ),
 
-              ////MaterialButton
+                ///CloseButton
+                const Text("CloseButton"),
+                CloseButton(onPressed: () {}, color: Colors.orangeAccent),
+                const SizedBox(
+                  height: 15,
+                ),
 
-              MaterialButton(
-                onPressed: () {},
-                child: const Text("MaterialButton"),
-              ),
+                ///DrawerButton
+                const Text("DrawerButton"),
+                DrawerButton(onPressed: () {}, style: const ButtonStyle()),
+                const SizedBox(
+                  height: 15,
+                ),
 
-              ///BackButton
-              BackButton(onPressed: () {}, color: Colors.deepOrange),
-
-              ///CloseButton
-              CloseButton(onPressed: () {}, color: Colors.orangeAccent),
-
-              ///DrawerButton
-              DrawerButton(onPressed: () {}, style: const ButtonStyle()),
-
-              ///DropdownButton
-              DropdownButton(
-                items: const [],
-                onChanged: (value) => 2,
-              ),
-            ])),
+                ///DropdownButton
+                const Text("DropdownButton"),
+                DropdownButton(
+                  items: const [],
+                  onChanged: (value) => 2,
+                ),
+              ])),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -7,119 +7,203 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Flutter Button "),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: (Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                /// ElevatedButton
+                const Text("ElevatedButton"),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orangeAccent,
+        
+                    /// Button Background Color
+                    foregroundColor: Colors.purple,
+        
+                    /// Button Text Color
+                    minimumSize: const Size(400, 80),
+        
+                    /// width = 300 ,height=40,
+                    maximumSize: const Size(400, 100),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 32),
+                    //padding: const EdgeInsets.all(32),
+                    // padding: const EdgeInsets.fromLTRB(32, 10, 32, 10),
+                    // padding: EdgeInsets.zero,
+                    // padding: const EdgeInsets.only(left: 0.0,top: 0.0,right: 0.0,bottom: 30),
+                    elevation: 7,
+                    shadowColor: Colors.deepOrange.shade400,
+        
+                    /// Other Option available
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+        
+                    /// Button Text Style
+                    textStyle: const TextStyle(
+                      color: Colors.deepOrange,
+        
+                      ///  Color No Apply,cause foreground color use
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.w800,
+                      wordSpacing: 10.0,
+                      letterSpacing: 5.0,
+                      decoration: TextDecoration.overline,
+        
+                      /// Other Option Available
+                      backgroundColor: Colors.white,
+        
+                      /// Button Text Background Color
+                    ),
+                  ),
+                  child: const Text("ElevatedButton"),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+        
+                /// TextButton
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+        
+                      /// Same to ElevatedButton Style apply.
+                      ),
+                  child: const Text("TextButton"),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                //// Icon Button
+                const Text("Icon Button"),
+                IconButton(
+                  onPressed: () {},
+                  style: IconButton.styleFrom(
+                      // Same To Elevated Button Style
+                      backgroundColor: Colors.orangeAccent),
+                  icon: const Icon(Icons.account_circle),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+        
+                ///FloatingActionButton with Label
+                const Text("FloatingActionButton with Label"),
+                FloatingActionButton.extended(
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  elevation: 8,
+                  backgroundColor: Colors.deepOrange,
+                  isExtended: true,
+                  hoverColor: Colors.white,
+                  autofocus: true,
+                  focusColor: Colors.amber.shade400,
+                  focusElevation: 10,
+                  label: const Text("FloatingActionButton"),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+        
+                ///FloatingActionButton with child
+                const Text("FloatingActionButton with child"),
+                FloatingActionButton(
+                  onPressed: () {},
+                  //child: const Text("FloatingActionButton"),
+                  child: const Icon(Icons.add_circle),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+        
+                ///FilledButton
+                const Text("FilledButton"),
+                FilledButton(
+                  style: FilledButton.styleFrom(
+                    /// same to Elevated Button style follow
+                    backgroundColor: Colors.amber,
+                  ),
+                  onPressed: () {},
+                  child: const Text("FilledButton"),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+        
+                /// OutlinedButton
+                const Text("OutlinedButton"),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: const Text("OutlinedButton"),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                ////MaterialButton
+                const Text("MaterialButton"),
+                MaterialButton(
+                  onPressed: () {},
+                  child: const Text("MaterialButton"),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+        
+                ///BackButton
+                const Text("BackButton"),
+                BackButton(onPressed: () {}, color: Colors.deepOrange),
+                const SizedBox(
+                  height: 15,
+                ),
+        
+                ///CloseButton
+                const Text("CloseButton"),
+                CloseButton(onPressed: () {}, color: Colors.orangeAccent),
+                const SizedBox(
+                  height: 15,
+                ),
+        
+                ///DrawerButton
+                const Text("DrawerButton"),
+                DrawerButton(onPressed: () {}, style: const ButtonStyle()),
+                const SizedBox(
+                  height: 15,
+                ),
+        
+                ///DropdownButton
+                const Text("DropdownButton"),
+                DropdownButton(
+                  items: const [],
+                  onChanged: (value) => 2,
+                ),
+              ])),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
