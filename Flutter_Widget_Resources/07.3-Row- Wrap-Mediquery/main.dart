3da30b275b3d8c2dd817fae9248ef96ffd259bf2
hafizurrahmanomar <hafizurrahmanomar@gmail.com>
2024-07-29 00:38:18 +0600

    Row-Wrap-Mediquery

--- Flutter_Widget_Resources/07.3-Row- Wrap-Mediquery/main.dart
+++ Flutter_Widget_Resources/07.3-Row- Wrap-Mediquery/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MyApp(),
-    );
+  MyApp(),
+);
 
 class MyApp extends StatelessWidget {
   @override
@@ -37,60 +37,52 @@ class _HomeScreenState extends State<HomeScreen> {
     print(MediaQuery.sizeOf(context));
     print(MediaQuery.orientationOf(context));
     print(MediaQuery.devicePixelRatioOf(context));
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Warp,MediaQuery,layout Builder'),
       ),
-      body: const Scrollbar(
-        thickness: 3.0,
-        radius: Radius.circular(10.0),
-        interactive: true,
-        trackVisibility: true,
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Scrollbar(
+            thickness: 7.0,
+            radius: Radius.circular(10.0),
+            interactive: true,
+            trackVisibility: true,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Text(
+                      "It is a long established fact that a reader will be distracted by the readable content"),
+                  Text("Hafizur Rhaman Omar"),
+                  Text("Hafizur Rhaman Omar"),
+                  Text("Hafizur Rhaman Omar"),
+                  Text(
+                      "It is a long established fact that a reader will be distracted by the readable content"),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 50.0,
+          ),
+          Wrap(
+            crossAxisAlignment: WrapCrossAlignment.start,
+            alignment: WrapAlignment.end,
+            runAlignment: WrapAlignment.start,
+            spacing: 10,
             children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                      "It is a long established fact that a reader will be distracted by the readable content"),
-                  Text("Hafizur Rhaman Omar"),
-                  Text("Hafizur Rhaman Omar"),
-                  Text("Hafizur Rhaman Omar"),
-                  Text(
-                      "It is a long established fact that a reader will be distracted by the readable content"),
-                ],
-              ),
-              SizedBox(
-                height: 50.0,
-              ),
-              Wrap(
-                crossAxisAlignment: WrapCrossAlignment.start,
-                alignment: WrapAlignment.end,
-                runAlignment: WrapAlignment.start,
-                spacing: 10,
-                children: [
-                  SizedBox(
-                    height: 50.0,
-                  ),
-                  Text("Hafizur Rhaman Omar,Hafizur Rhaman Omar Hafizur Rhaman Omar-01"),
-                  Text("Hafizur Rhaman Omar,Hafizur Rhaman Omar Hafizur Rhaman Omar-02"),
-                  Text("Hafizur Rhaman Omar,Hafizur Rhaman Omar Hafizur Rhaman Omar-03"),
-                  Text("Hafizur Rhaman Omar,Hafizur Rhaman Omar Hafizur Rhaman Omar-04"),
-                  Text("Hafizur Rhaman Omar,Hafizur Rhaman Omar Hafizur Rhaman Omar-05"),
-
-                ],
-              ),
+              Text(
+                  "Hafizur Rhaman Omar,Hafizur Rhaman Omar Hafizur Rhaman Omar"),
+              Text("Hafizur Rhaman Omar"),
+              Text("Hafizur Rhaman Omar"),
+              Text("Hafizur Rhaman Omar"),
+              Text("Hafizur Rhaman Omar"),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
 }
-
